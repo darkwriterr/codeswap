20805af1a14e623b63e59d2a95fe1da0622e142e
darkwriterr <ulanovskyimark@gmail.com>
2025-06-25 04:18:47 +0300

    fix: callback URL

--- backend/index.js
+++ backend/index.js
@@ -233,7 +233,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
+  callbackURL: "https://codeswap-3jvp.onrender.com/auth/google/callback",
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const email = profile.emails[0].value;
